60188c55d24c8d3f381ced10534cdf1dc22c902a
breduffy <breanne.duffy@gmail.com>
2015-06-07 16:16:00 -0400

    Fixed the link to Amazon web services in the artwork serializer.

--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -2,6 +2,6 @@ class ArtworkSerializer < ActiveModel::Serializer
   attributes :url, :title, :city, :description, :neighborhood, :artist
 
   def url
-    "https://s3.amazonaws.com/bostonartgallery/medium/#{object.id}/#{object.image_file_name}"
+    "https://s3.amazonaws.com/bostonartgallery/artworks/medium/#{object.id}/#{object.image_file_name}"
   end
 end
